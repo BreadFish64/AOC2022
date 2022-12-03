955d7acaad64002d5c303772bca49910e60113b2
Marshall Mohror <mohror64@gmail.com>
2022-12-03 00:33:20 -0600

    Day 3 golf

--- AOC/rucksack_reorganization.cpp
+++ AOC/rucksack_reorganization.cpp
@@ -1,35 +1,20 @@
 #include "pch.hpp"
 
-constexpr int ToPriority(char c) {
-    if (c >= 'a') {
-        return 1 + c - 'a';
-    } else {
-        return 27 + c - 'A';
-    }
+constexpr u64 ToPriority(char c) {
+    return 1_u64 << ((c >= 'a') ? (1 + c - 'a') : (27 + c - 'A'));
 }
-constexpr u64 CompartmentContents(auto&& compartment) {
-    u64 contents = 0;
-    for (char item : compartment)
-        contents |= 1_u64 << ToPriority(item);
-    return contents;
+constexpr u64 CompartmentContents(std::string_view compartment) {
+    return ranges::accumulate(compartment, 0_u64, std::bit_or{}, ToPriority);
 }
 int main() {
     std::ifstream input_file{"input.txt"};
     std::vector   input(std::istream_iterator<std::string>{input_file}, {});
-
-    u64 sum = ranges::accumulate(input | views::transform([](auto&& rucksack) {
-                                     auto common = CompartmentContents(rucksack | views::take(rucksack.size() / 2));
-                                     common &= CompartmentContents(rucksack | views::drop(rucksack.size() / 2));
-                                     return std::countr_zero(common);
-                                 }),
-                                 0_u64);
-    fmt::print("Part 1: {}\n", sum);
-    sum = ranges::accumulate(input | views::chunk(3) | views::transform([](auto&& sacks) {
-                                 auto common = ~0_u64;
-                                 for (auto&& rucksack : sacks)
-                                     common &= CompartmentContents(rucksack);
-                                 return std::countr_zero(common);
-                             }),
-                             0_u64);
-    fmt::print("Part 2: {}\n", sum);
+    u64           sum1 = ranges::accumulate(input, 0_u64, std::plus{}, [](auto&& rucksack) {
+        return std::countr_zero(CompartmentContents(rucksack | views::take(rucksack.size() / 2)) &
+                                          CompartmentContents(rucksack | views::drop(rucksack.size() / 2)));
+    });
+    u64           sum2 = ranges::accumulate(input | views::chunk(3), 0_u64, std::plus{}, [](auto&& sacks) {
+        return std::countr_zero(ranges::accumulate(sacks, ~0_u64, std::bit_and{}, CompartmentContents));
+    });
+    fmt::print("Part 1: {}\nPart 2: {}\n", sum1, sum2);
 }
